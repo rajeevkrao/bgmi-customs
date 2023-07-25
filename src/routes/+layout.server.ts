4a5af64d832a240e134c8ee3a606baef1e88d514
Your Name <you@example.com>
2023-07-26 02:27:59 +0530

    prerender for clientAddress fix

--- src/routes/+layout.server.ts
+++
@@ -1,5 +0,0 @@
-import type { ServerLoad } from '@sveltejs/kit';
-
-export const load: ServerLoad = async (params) => {
-	console.log({clientAddress:params.getClientAddress()});
-};
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -35,21 +35,21 @@
 		box-sizing: border-box;
 	}
 
-	footer {
+	/* footer {
 		display: flex;
 		flex-direction: column;
 		justify-content: center;
 		align-items: center;
 		padding: 12px;
-	}
+	} */
 
-	footer a {
+	/* footer a {
 		font-weight: bold;
-	}
+	} */
 
-	@media (min-width: 480px) {
+	/* @media (min-width: 480px) {
 		footer {
 			padding: 12px 0;
 		}
-	}
+	} */
 </style>
